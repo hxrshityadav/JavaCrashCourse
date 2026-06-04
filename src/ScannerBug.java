185be5504c3fcdcd6db808a0a0220ae6056d07a3
hxrshityadav <harshityadavgkp@gmail.com>
2026-06-04 19:34:01 +0530

    Scanner Class

---
+++ src/ScannerBug.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class ScannerBug {
+    public static void main(String[] args){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter age: ");
+        int age = sc.nextInt(); // nextInt() leaves '\n' in the buffer
+
+        sc.nextLine(); //consume leftover newline
+
+        System.out.println("Enter name: ");
+        String name = sc.nextLine();
+
+        System.out.println("Age: " + age);
+        System.out.println("Name: " + name);
+
+        sc.close();
+    }
+}
