59487faa14e72916242b9a374afdb565f330ad76
hxrshityadav <harshityadavgkp@gmail.com>
2026-06-04 17:59:32 +0530

    Increment and Decrement Operator

---
+++ src/IncrementAndDecrementOperators.java
@@ -0,0 +1,9 @@
+public class IncrementAndDecrementOperators {
+    public static void main(String[] args){
+        int n = 5;
+        System.out.println(n++);
+        System.out.println(n--);
+        System.out.println(++n);
+        System.out.println(--n);
+    }
+}
