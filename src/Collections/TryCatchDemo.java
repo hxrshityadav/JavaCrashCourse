20c815178ce15466dc7260f63fff3d92d95a84c8
hxrshityadav <harshityadavgkp@gmail.com>
2026-06-16 01:41:47 +0530

    Exception Handling, Wrapper Classes, and File Handling

---
+++ src/Collections/TryCatchDemo.java
@@ -0,0 +1,19 @@
+package Collections;
+
+public class TryCatchDemo {
+
+    public static void main(String[] args) {
+
+        try {
+
+            int result = 10 / 0;
+
+            System.out.println(result);
+
+        } catch(Exception e) {
+
+            System.out.println("Cannot divide by zero!");
+
+        }
+    }
+}
